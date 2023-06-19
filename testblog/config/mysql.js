eac58279e62b282687996846b0886d9f3b0474b3
chorice0915 <chorice0915@gmail.com>
2023-06-19 13:46:29 +0900

    2023 수행평가

---
+++ testblog/config/mysql.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: 'localhost',
+  user: 'testblog',
+  password : '1234',
+  database: 'testblog',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, 
+  idleTimeout: 60000, 
+  queueLimit: 0
+}
---
+++ testblog/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const authHandler = require('../handlers/auth.js');
+
+router.get('/signIn', (req, res)=>{ res.render('auth/signIn.html');});
+router.post('/signInProcess', authHandler.signInProcess);
+
+router.get('/signUp', (req, res)=>{res.render('auth/signUp.html');});
+router.post('/signUpProcess', authHandler.signUpProcess);
+
+router.get('/signOut', authHandler.signOut);
+
+
+module.exports = router;
